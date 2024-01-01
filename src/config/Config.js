3a409db23b1bb96e86037d2948d64b32a232a474
reiyua <rei@reiyua.lol>
2024-01-01 12:47:23 +0000

    Configre Google Firebase

---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDHIdFwe8bMElvJ2oRpwiT-sbQzWkq5X4U",
+    authDomain: "braycedforunbased.firebaseapp.com",
+    projectId: "braycedforunbased",
+    storageBucket: "braycedforunbased.appspot.com",
+    messagingSenderId: "345898582027",
+    appId: "1:345898582027:web:de0f642ece34f9517f070e"
+  };
